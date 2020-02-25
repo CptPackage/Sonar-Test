76d079f313f2ff25bf76657f7dd071487cd8b511
CptPackage <abdelrhmanabdalla15@gmail.com>
2020-02-25 16:32:52 +0100

    Added travis support

---
+++ src/logic/App.java
@@ -0,0 +1,11 @@
+package logic;
+
+import java.util.logging.Logger;
+
+public class App {
+
+	public static void main(String[] args) {
+		Logger.getAnonymousLogger().info("Testing sonar cloud!");
+	}
+	
+}
